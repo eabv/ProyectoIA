8e48bfabe3420e1728ed1418c4b03929cf5a7632
MaylonCuzco <mecuzco@espe.edu.ec>
2022-06-10 00:15:59 -0500

    Se crea el archivo en donde se programará el agente de aire acondicionado.

---
+++ agente.py
@@ -0,0 +1,7 @@
+import random
+class Casas(object):
+    def __init__(self):
+        self.ubicacionestado =  {'C1': random.randint(0,1), 'C2': random.randint(0,1), 'C3': random.randint(0,1), 
+                            'C4': random.randint(0,1), 'C5': random.randint(0,1), 'C6': random.randint(0,1),
+                            'C7': random.randint(0,1)}
+
